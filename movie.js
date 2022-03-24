8bd5bb2985f0ebf512913d1d4fabffe70eccb556
Oscar Castro <oscar.a.castro818@gmail.com>
2022-03-24 01:11:33 -0500

    added button click events for search bar refactored api function

--- movie.js
+++ movie.js
@@ -4,12 +4,14 @@ const mapElementToOption = (movie) => `<option value="${movie.id}">${movie.Title
 
 const mapElementToDiv = (movie) => `<div id="movies${movie.id}" class="movie-card">
                 <div><img src="${movie.Poster}"></div>
-                <div class="content hide">${movie.Title}</div>
-                <div class="content hide">Rating: ${movie.Rating}</div>
-                <div class="content hide">Year: ${movie.Year}</div>
-                <div class="content hide">Genre: ${movie.Genre}</div>
-                <div class="content hide">Director: ${movie.Director}</div>
-                <div class="content hide">Plot: ${movie.Plot}</div>
+                <div class="title content hide">${movie.Title}</div>
+                <div class="content hide"><strong>Rating:</strong> ${movie.Rating}</div>
+                <div class="content hide"><strong>Year:</strong> ${movie.Year}</div>
+                <div class="content hide"><strong>Genre:</strong> ${movie.Genre}</div>
+                <div class="content hide"><strong>Director:</strong> ${movie.Director}</div>
+                <div class="content hide"><strong>Actors:</strong> ${movie.Actors}</div>
+                <div class="content hide"><strong>Plot:</strong> ${movie.Plot}</div>
+                <div class="content hide d-none">Poster:</strong> ${movie.Poster}</div>
                 <button class="content hide btn-dark" type="button" id="delete-movie" onclick="deleteMovie(${movie.id})">Delete Movie</button>
             </div>`;
 
@@ -19,23 +21,19 @@ const moviesUrl = "https://scientific-thoughtful-asparagus.glitch.me/movies";
 function getMovies() {
     fetch(moviesUrl).then(resp => resp.json()).then(function(data){
 
-        const movies = data.reverse().map(mapElementToDiv)
-        $('#output-container').html(movies)
+        const movies = data.reverse().map(mapElementToDiv);
+        $('#output-container').html(movies);
 
-        const moviesOption = data.map(mapElementToOption)
-        $('#movies-list').html(moviesOption)
+        const moviesOption = data.map(mapElementToOption);
+        $('#movies-list').html(moviesOption);
+        $('#movies-list').prepend(`<option selected>Open this select menu</option>`);
 
         data.forEach(function (movie){
             $(`#movies${movie.id}`).click(function (){
-                $(this).children().toggleClass('hide')
+                $(this).children().toggleClass('hide');
                 // $(this).children('.content ').slideToggle(500)
             });
-        })
-        // $(this).children('.content ').slideToggle(500)
-        // $(".movie-card").click(function (e){
-        //     console.log(e);
-        //     $(".movie-content").toggleClass('movie-content')
-        // });
+        });
     });
 }
 getMovies();
@@ -45,7 +43,13 @@ $('#add-movie-btn').click(function (e){
 
     const movieToPost = {
         Title: $('#movie-title').val(),
-        Rating: $('#movie-rating').val()
+        Rating: $('#movie-rating').val(),
+        Year: $('#movie-year').val(),
+        Genre: $('#movie-genre').val(),
+        Director: $('#movie-director').val(),
+        Actors: $('#movie-actors').val(),
+        Poster: $('#movie-poster').val(),
+        Plot: $('#movie-plot').val()
     };
     const postOptions = {
         method: 'POST',
@@ -78,6 +82,12 @@ $('#movies-list').change(function (){
                 console.log(selectedMovie);
                 $('#edit-movie-title').val(movie.Title)
                 $('#edit-movie-rating').val(movie.Rating)
+                $('#edit-movie-year').val(movie.Year);
+                $('#edit-movie-genre').val(movie.Genre);
+                $('#edit-movie-director').val(movie.Director);
+                $('#edit-movie-actors').val(movie.Actors);
+                $('#edit-movie-poster').val(movie.Poster);
+                $('#edit-movie-plot').val(movie.Plot);
             }
         });
     });
@@ -93,7 +103,14 @@ $('#edit-movie-btn').click(function (e){
 function editMovie(id) {
     const movieToPost = {
         Title: $('#edit-movie-title').val(),
-        Rating: $('#edit-movie-rating').val()
+        Rating: $('#edit-movie-rating').val(),
+        Year: $('#edit-movie-year').val(),
+        Genre: $('#edit-movie-genre').val(),
+        Director: $('#edit-movie-director').val(),
+        Actors: $('#edit-movie-actors').val(),
+        Poster: $('#edit-movie-poster').val(),
+        Plot: $('#edit-movie-plot').val()
+
     };
     const putOptions = {
         method: 'PUT',
@@ -108,18 +125,30 @@ function editMovie(id) {
 function clearValue () {
     $('#movie-title').val('');
     $('#movie-rating').val('');
+    $('#movie-year').val('');
+    $('#movie-genre').val('');
+    $('#movie-director').val('');
+    $('#movie-actors').val('');
+    $('#movie-poster').val('');
+    $('#movie-plot').val('')
     $('#edit-movie-title').val('');
     $('#edit-movie-rating').val('');
+    $('#edit-movie-year').val('');
+    $('#edit-movie-genre').val('');
+    $('#edit-movie-director').val('');
+    $('#edit-movie-actors').val('');
+    $('#edit-movie-poster').val('');
+    $('#edit-movie-plot').val('');
     $('#api-input').val('')
 }
 
 // //API:
-// function getMoviePoster() {
-    $("#api-btn").click(function (e){
-        e.preventDefault();
-        let userInput = $("#api-input").val()
-        console.log(userInput);
-        $.get("http://omdbapi.com/?apikey=" + [MOVIE_API] + "&t=" + userInput, {
+function getApiData(API) {
+//     $("#api-btn").click(function (e){
+//         e.preventDefault();
+//         let userInput = $("#api-input").val()
+//         console.log(userInput);
+        $.get("http://omdbapi.com/?apikey=" + [MOVIE_API] + "&t=" + API, {
         }).done(function (data) {
             console.log(data);
             console.log(data.Ratings[0].Value);
@@ -132,6 +161,7 @@ function clearValue () {
                 Year: data.Year,
                 Genre: data.Genre,
                 Director: data.Director,
+                Actors: data.Actors
             };
             const postOptions = {
                 method: 'POST',
@@ -141,7 +171,7 @@ function clearValue () {
                 body: JSON.stringify(movieToPost)
             };
             fetch(moviesUrl, postOptions).then(getMovies);
-            clearValue()
+            clearValue();
             // $("#staticBackdrop").css({
             //     "display": "none",
             //     "z-index": "0"
@@ -151,8 +181,26 @@ function clearValue () {
             //     "z-index": "0"
             // });
          //SAVE SPOT>
-        });
     });
+}
+
+$("#api-btn").click(function (e){
+    e.preventDefault();
+    const userInput = $("#api-input").val()
+    getApiData(userInput);
+});
+
+$('#search-bar-btn').click(function (e) {
+    e.preventDefault();
+    const userInput = $("#search-bar").val();
+    getApiData(userInput);
+    $("#search-bar").val('');
+});
 
 
-
+$('#edit-movie-close').click(function (){
+    clearValue();
+});
+$('#add-movie-close').click(function (){
+    clearValue();
+});
